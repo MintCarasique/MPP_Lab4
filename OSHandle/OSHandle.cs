ee90d37e8a27b91d07d7a98d779cefe4502fba00
Artem Slizhevskiy <mykew98@gmail.com>
2017-11-16 22:07:42 +0300

    Added realization

---
+++ OSHandle/OSHandle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSHandle
+{
+    public abstract class OSHandle<T>: IDisposable
+    {
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected void Dispose(bool disposing)
+        {
+            if (!AlreadyDisposed)
+            {
+                if (disposing)
+                {
+                    var disposable = Resource as IDisposable;
+                    disposable?.Dispose();
+                }
+
+                if (Handle != IntPtr.Zero)
+                {
+                    Marshal.FreeHGlobal(Handle);
+                    Handle = IntPtr.Zero;
+                }
+                AlreadyDisposed = true;
+            }
+        }
+
+        public IntPtr Handle { get; set; }
+
+        protected bool AlreadyDisposed { get; set; }
+
+        public T Resource { get; set; }
+    }
+}
